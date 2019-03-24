bc1a84dfc1664be9363b609a6b3e790867982598
宋汉仑 <songhanlun@sous-MBP.fios-router.home>
2019-03-23 22:40:22 -0400

    Add a simple page

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
 const port = 56566
 
-app.get("/", (req, res) => res.send("Hello RasPi!"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
 
 app.listen(port, () => console.log(`Raspi main app listening on port ${port}!`))
